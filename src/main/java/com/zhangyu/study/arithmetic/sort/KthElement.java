461360efb28d56c66cbcfc109f85b0d0c117fa63
zhangyu <zhangyu9069@shein.com>
2019-11-22 18:20:14 +0800

    KthElement排序

---
+++ src/main/java/com/zhangyu/study/arithmetic/sort/KthElement.java
@@ -0,0 +1,50 @@
+package com.zhangyu.study.arithmetic.sort;
+
+/**
+ * @author zhangyu
+ * @description
+ * @date 2019/11/22
+ */
+public class KthElement {
+
+    public static void main(String[] args) {
+        int[] nums = {1,42,2,6,4,8};
+        findKthLargest(nums, 2);
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        k = nums.length - k;
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int j = partition(nums, l, h);
+            if (j == k) {
+                break;
+            } else if (j < k) {
+                l = j + 1;
+            } else {
+                h = j - 1;
+            }
+        }
+        return nums[k];
+    }
+
+    private static int partition(int[] a, int l, int h) {
+        int i = l, j = h + 1;
+        while (true) {
+            while (a[++i] < a[l] && i < h) ;
+            while (a[--j] > a[l] && j > l) ;
+            if (i >= j) {
+                break;
+            }
+            swap(a, i, j);
+        }
+        swap(a, l, j);
+        return j;
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
